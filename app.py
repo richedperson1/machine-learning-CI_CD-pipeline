7ae1306b232c69e2a3d7141f2f8ae5fc373e98dc
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-09-14 15:37:50 +0530

    Added exception class and logger class module in the project

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
 from flask import *
 from housing.logger import logging
+from housing.exception import housingExeption
+
+import sys
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     try:
+        raise Exception("hello word")
         logging.info("we are under testing phase")
         return "Hello world"
-    except:
-        return "Server not responding"
+    except  Exception as e:
+        housing = housingExeption(e,sys)
+        logging.info(housing.errorMessage)
+    return "Server not responding"
 
 if __name__=="__main__":
     app.run(debug=True,host="0.0.0.0",port=5000)
