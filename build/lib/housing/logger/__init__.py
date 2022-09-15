d16ffe7af5edcbbf147a7376efb6e33f6682210e
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2022-09-15 15:54:33 +0530

    Added and remove all necessary and unnecessary file from the directory

--- build/lib/housing/logger/__init__.py
+++ build/lib/housing/logger/__init__.py
@@ -1,19 +1,16 @@
 import logging
 from datetime import *
 import os
-WORKDIR = "\housingLongs"
+
+WORKDIR = "housingLogs"
 
 currentTimeStamp = f'{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
 
 logFileName = f"log_{currentTimeStamp}.log"
-try:
-    os.makedirs(WORKDIR)
-except:
-    pass
+os.makedirs(WORKDIR,exist_ok=True)
 
-filen = "rutvik.log"
-logFilePath = os.getcwd()+ os.path.join(WORKDIR,filen)
+logFilePath = os.getcwd()+"\\"+ os.path.join(WORKDIR,logFileName)
 
-logging.basicConfig(filename=logFilePath,filemode="w",format='[%(sctime)s -%(name)s -%(levelname)s -%(message)s]',level=logging.INFO)
+logging.basicConfig(filename=logFilePath,filemode="w",format='[%(asctime)s -%(name)s -%(levelname)s -%(message)s]',level=logging.INFO)
 
 
